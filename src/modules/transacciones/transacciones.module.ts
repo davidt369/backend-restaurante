0ab75e8cf6f7bba3005aea68ed52dbc2f3f0d0b1
Marcelo Flores <mjfbchelo@gmail.com>
2026-03-23 08:58:41 -0400

    Actualizar TransaccionesModule con servicios especializados
    - Importar los 9 servicios especializados
    - Registrar servicios en providers
    - Exportar TransaccionesService como la interfaz publica

--- src/modules/transacciones/transacciones.module.ts
+++ src/modules/transacciones/transacciones.module.ts
@@ -5,10 +5,34 @@ import { CocinaGateway } from './cocina.gateway';
 import { DrizzleModule } from '../../drizzle/drizzle.module';
 import { CajaModule } from '../caja/caja.module';
 
+// Servicios especializados
+import { TransaccionesCoreService } from './services/transacciones-core.service';
+import { ItemsService } from './services/items.service';
+import { ExtrasService } from './services/extras.service';
+import { PagosService } from './services/pagos.service';
+import { CalculosFinancierosService } from './services/calculos-financieros.service';
+import { EstadosTransaccionService } from './services/estados-transaccion.service';
+import { CocinaIntegrationService } from './services/cocina-integration.service';
+import { StockService } from './services/stock.service';
+import { CajaReportesService } from './services/caja-reportes.service';
+
 @Module({
   imports: [DrizzleModule, CajaModule],
   controllers: [TransaccionesController],
-  providers: [TransaccionesService, CocinaGateway],
+  providers: [
+    TransaccionesService,
+    CocinaGateway,
+    // Servicios especializados
+    TransaccionesCoreService,
+    ItemsService,
+    ExtrasService,
+    PagosService,
+    CalculosFinancierosService,
+    EstadosTransaccionService,
+    CocinaIntegrationService,
+    StockService,
+    CajaReportesService,
+  ],
   exports: [TransaccionesService],
 })
 export class TransaccionesModule {}
